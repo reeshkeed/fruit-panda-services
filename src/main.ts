f44b8aa60bed8a46ed69d7c4b2e66a6bb3268ee7
reeshkeed <hello@girishdaloso.com>
2023-01-14 23:06:37 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const globalPrefix = process.env.APP_GLOBAL_PREFIX || 'api';
+  const swaggerPrefix = process.env.APP_SWAGGER_PREFIX || 'documentation';
+  const port = process.env.APP_PORT || 3333;
+
+  const app = await NestFactory.create(AppModule);
+
+  // Set api prefix
+  app.setGlobalPrefix(globalPrefix);
+
+  // Swagger configuration build
+  const config = new DocumentBuilder()
+    .setTitle('FruitPanda API')
+    .setDescription('Fruit Panda API Documentation')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(swaggerPrefix, app, document);
+
+  await app.listen(port, () => {
+    Logger.log(`Listening: http://localhost:${port}/${globalPrefix}`);
+    Logger.log(`API Documentation: http://localhost:${port}/${swaggerPrefix}`);
+  });
+}
+bootstrap();
